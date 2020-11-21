ffc7876a735ff55241a16ea3dec127086b249298
Nehagarg0815 <73516560+Nehagarg0815@users.noreply.github.com>
2020-11-21 18:17:21 +0530

    Selenium Suite Example

---
+++ suiteExample/DemoOne.java
@@ -0,0 +1,14 @@
+package suiteExample;
+
+import org.testng.annotations.Test;
+
+public class DemoOne {
+  @Test
+  public void firstTestCase() {
+	  System.out.println("This is first test case from class DemoOne");
+  }
+  @Test
+  public void secondtestcase() {
+	  System.out.println("This is second test case from class DemoOne");
+  }
+}
