73cc0a0afb41434f50a0fbbe2695de7c90f30c3c
Nehagarg0815 <73516560+Nehagarg0815@users.noreply.github.com>
2020-11-27 15:34:13 +0530

    TestNG Activity4_1

---
+++ TestNG/DemoTwo.java
@@ -0,0 +1,10 @@
+package suiteExample;
+
+import org.testng.annotations.Test;
+
+public class DemoTwo {
+  @Test
+  public void testcase() {
+	  System.out.println("This is testcase from Demotwo class");
+  }
+}
